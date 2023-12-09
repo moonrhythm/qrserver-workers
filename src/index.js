58de790a4676804661f00b799b6c9c17d3df834f
Thanatat Tamtan <acoshift@gmail.com>
2023-12-09 16:10:25 +0700

    update wrangler

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ const qr = require('qr-image')
 export default {
 	async fetch(request, env, ctx) {
 		const url = new URL(request.url)
-		
-		if (url.pathname !== '/') {
-			return new Response('404 page not found', { status: 404 })
-		}
-		
+
 		const content = url.searchParams.get('c') || ''
 		let l = url.searchParams.get('l') || ''
 		let s = +url.searchParams.get('s') || undefined
 		let ext = url.searchParams.get('ext') || ''
-		
+
 		if (!content) {
 			return new Response('empty content', { status: 400 })
 		}
@@ -25,15 +21,15 @@ export default {
 		if (!s) {
 			s = ext === 'png' ? 4 : 1
 		}
-		
+
 		console.log(`generate: c=${content}, ext=${ext}, l=${l}, s=${s}`)
-		
+
 		const img = qr.imageSync(content, {
 			type: ext,
 			ec_level: l,
 			size: s
 		})
-		
+
 		return new Response(img, {
 			headers: {
 				'content-type': {
